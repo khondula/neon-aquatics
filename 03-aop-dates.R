88b84c573ed8dfd4babf2dabe8a8b73f4b5010a0
kelly hondula <hondula@battelleecology.org>
2021-01-28 17:10:31 -0500

    flight dates x doc plots

--- 03-aop-dates.R
+++ 03-aop-dates.R
@@ -17,8 +17,10 @@ aop_dates <- aop_dates %>%
          flightdate = ymd(substr(FlightDate, 1, 8)))
 aop_dates %>% write_csv('results/all_aop_dates.csv')
 
+# aop_dates <- read_csv('results/all_aop_dates.csv')
+# sites_x_aop <- read_csv('results/sites_x_aop.csv')
 sites_join_aop_dates <- sites_x_aop %>% left_join(aop_dates, by = c("aop_site_id" = "siteid"))
-
+sites_join_aop_dates %>% write_csv('results/sites_join_aop_dates.csv')
 # aquatic to aop sites
 
 ### Or from API ###
--- 05-plots-swchem.R
+++ 05-plots-swchem.R
@@ -1,4 +1,7 @@
 # boxplots swchem by site
+library(tidyverse)
+library(colorspace)
+library(lubridate)
 
 doc_df <- read_csv('results/doc_all.csv')
 
@@ -14,16 +17,22 @@ doc_df %>%
 ggsave('figs/doc-x-site.png')  
 
 # time series with flight dates
+sites_join_aop_dates <- read_csv('results/sites_join_aop_dates.csv')
+
 doc_df %>%
   left_join(sites_x_aop, by = c("namedLocation", "siteID", "domainID" = "domanID")) %>%
   mutate(date = as_date(collectDate)) %>%
-  ggplot(aes(x = date, y = analyteConcentration, group = namedLocation)) +
+  ggplot(aes(x = date, y = analyteConcentration)) +
+  geom_vline(data = sites_join_aop_dates, aes(xintercept = flightdate), col = "red") +
   geom_line(aes(col = sitetype)) + 
   geom_point(aes(fill = sitetype), pch = 21) +
   theme_bw() +
   xlab(element_blank()) +
   ylab("DOC (mg/L)") +
+  expand_limits(y = 0) +
   theme(legend.position = "none") +
+  scale_color_discrete_sequential("Batlow") +
+  scale_fill_discrete_sequential("Batlow") +
   facet_wrap(vars(siteID), scales = "free_y")
 
 ggsave('figs/doc-timeseries.png', height = 10, width = 18)  
