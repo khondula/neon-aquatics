4fa0e076e42dd6099f4895f250fb4f5bf127ed47
kelly hondula <hondula@battelleecology.org>
2021-02-01 18:02:25 -0500

    mega function for sensor data

--- 07-sensor-data.R
+++ 07-sensor-data.R
@@ -9,54 +9,76 @@ library(vroom)
 
 # test out with COMO data
 wq_dir <- '~/Box/data/NEON/NEON_water-quality'
-siteid <- 'COMO'
 ts_dir <- '~/Box/data/NEON/wq-timeseries'
 # pull in data for one site (COMO)
 # AFTER moved all files to folder in 01
 
-wq_coltypes <- "TTddiddiddiddiddiddiddidddii"
+# water quality sensor data
+# downloaded everything for one site to test
+wq_dir <- '~/Box/data/NEON/NEON_water-quality 2'
+siteid <- 'ARIK'
 
-# read all files in together
-wq_df <- glue('{wq_dir}/{siteid}') %>%
-  fs::dir_ls(glob = "*waq_instantaneous*") %>% 
-  purrr::map_dfr(~read_csv(.x, col_types = wq_coltypes), .id = 'filename') %>%
-  dplyr::mutate(filename = basename(filename)) %>% 
-  dplyr::mutate(siteid = substr(filename, 10, 13),
-         sensor_position = substr(filename, 29, 33))
+# first move all data from a site into one folder with site name
 
-# then save separate time series for each sensor
-# SC, DO, DOsat, pH, chl, turb, fDOM
-
-wq_params <- list(specificConductance = names(wq_df)[7:9],
-                  dissolvedOxygen = names(wq_df)[10:12],
-                  dissolvedOxygenSaturation = names(wq_df)[13:15],
-                  pH = names(wq_df)[16:18],
-                  chlorophyll = names(wq_df)[19:21],
-                  turbidity = names(wq_df)[22:24],
-                  fDOM = names(wq_df)[25:28])
-
-# function to save separate CSV for each parameter and sensor
-wq_param <- 'fDOM'
-save_sensor_wq_timeseries <- function(wq_param){
-  # list of parameter data for each sensor
-  wq_par_list <- wq_df %>% 
-    filter(!is.na(wq_params[[wq_param]][1])) %>%
-    dplyr::select(siteid, sensor_position, startDateTime, 
-                  endDateTime, wq_params[[wq_param]]) %>%
-    group_by(sensor_position) %>% group_split()
-  # name list with each sensor
-  sensorids <- wq_par_list %>% 
-    purrr::map_chr(~unique(pull(.x, sensor_position)))
-  names(wq_par_list) <- sensorids
-  # make directory and save files
-  site_ts_dir <- glue('{ts_dir}/{siteid}')
-  fs::dir_create(site_ts_dir)
-  filenames <- glue('{site_ts_dir}/{siteid}_{wq_param}_sensor{sensorids}.csv')
-  wq_par_list %>% 
-    purrr::walk2(.y = filenames, ~vroom_write(.x, .y, delim = ","))
-  wq_par_list %>% purrr::map(~nrow(.x))
-  message()
+rearrange_sensor_data <- function(wq_dir, siteid){
+  # rearrange downloaded files into one directory
+  mydirs <- fs::dir_ls(wq_dir, glob = glue('*{siteid}*'))
+  myfiles <- fs::dir_ls(mydirs, recurse = 1)
+  fs::dir_create(glue('{wq_dir}/{siteid}'))
+  myfiles_new <- myfiles %>% map_chr(~glue('{wq_dir}/{siteid}/{basename(.x)}'))
+  purrr::walk2(.x = myfiles, .y = myfiles_new, ~fs::file_move(.x, .y))
+  fs::dir_delete(mydirs)
+  
+  # then read all files in together
+  wq_coltypes <- "TTddiddiddiddiddiddiddidddii"
+  wq_df <- glue('{wq_dir}/{siteid}') %>%
+    fs::dir_ls(glob = "*waq_instantaneous*") %>% 
+    purrr::map_dfr(~read_csv(.x, col_types = wq_coltypes), .id = 'filename') %>%
+    dplyr::mutate(filename = basename(filename)) %>% 
+    dplyr::mutate(siteid = substr(filename, 10, 13),
+                  sensor_position = substr(filename, 29, 33))
+  
+  # then save separate time series for each sensor
+  # SC, DO, DOsat, pH, chl, turb, fDOM
+  wq_params <- list(specificConductance = names(wq_df)[7:9],
+                    dissolvedOxygen = names(wq_df)[10:12],
+                    dissolvedOxygenSaturation = names(wq_df)[13:15],
+                    pH = names(wq_df)[16:18],
+                    chlorophyll = names(wq_df)[19:21],
+                    turbidity = names(wq_df)[22:24],
+                    fDOM = names(wq_df)[25:28])
+  
+  #  then save separate CSV for each parameter and sensor
+  # need to have wq_df, wq_params, ts_dir, siteid
+  save_sensor_wq_timeseries <- function(wq_param){
+    # list of parameter data for each sensor
+    wq_par_list <- wq_df %>% 
+      filter(!is.na(wq_params[[wq_param]][1])) %>%
+      dplyr::select(siteid, sensor_position, startDateTime, 
+                    endDateTime, wq_params[[wq_param]]) %>%
+      group_by(sensor_position) %>% group_split()
+    # name list with each sensor
+    sensorids <- wq_par_list %>% 
+      purrr::map_chr(~unique(pull(.x, sensor_position)))
+    names(wq_par_list) <- sensorids
+    # make directory and save files
+    site_ts_dir <- glue('{ts_dir}/{siteid}')
+    fs::dir_create(site_ts_dir)
+    filenames <- glue('{site_ts_dir}/{siteid}_{wq_param}_sensor{sensorids}.csv')
+    wq_par_list %>% 
+      purrr::walk2(.y = filenames, ~vroom_write(.x, .y, delim = ","))
+    wq_par_list %>% purrr::map(~nrow(.x))
+  }
+  
+  names(wq_params) %>% purrr::walk(~save_sensor_wq_timeseries(.x))
+  
 }
+
+
+rearrange_sensor_data(wq_dir = "~/Box/data/NEON/NEON_water-quality 2", "ARIK")
+
+
+
 
 
 
