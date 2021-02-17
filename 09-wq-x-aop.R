99d6259c49be61832d71a73e0ed47d71ed6cdd30
kelly hondula <hondula@battelleecology.org>
2021-02-17 13:57:35 -0500

    rename since this generalizes to all wq sensor data

--- 09-wq-x-aop.R
+++ 09-wq-x-aop.R
@@ -50,49 +50,48 @@ aop_dates_df <- data.frame(startDateTime = lubridate::as_datetime(aop_dates)) %>
 
 # filter out all flagged data
 wq_df_qa <- wq_df_all %>% 
-  filter(fDOMFinalQF %in% 0) %>% head() %>%
   dplyr::filter(!!sym(myQFcol) %in% 0)
-suna_qa_dates <- suna_df_qa$date %>% unique()
+wq_qa_dates <- wq_df_qa$date %>% unique()
 
-suna_df_all %>%
-  ggplot(aes(x = startDateTime, y = surfWaterNitrateMean)) +
+wq_df_all %>% 
+  ggplot(aes(x = startDateTime, y = !!sym(myparam))) +
   geom_vline(data = aop_dates_df, aes(xintercept = startDateTime),
              col = 'red', lwd = 0.5) +
   geom_point(size = 0.5, alpha = 0.5, col = 'gray') +
-  geom_point(data = suna_df_qa, size = 0.5) +
-  # facet_wrap(vars(year), scales = 'free_x', ncol =1) +
+  geom_point(data = wq_df_qa, size = 0.5) +
+  facet_wrap(vars(sensor_position), ncol =1) +
   theme_minimal() +
-  ggtitle(glue('SUNA and flights at {mysite}'))
+  ggtitle(glue('{myparam} and flights at {mysite}'))
 
-ggsave(glue('figs/suna-x-aop/{mysite}_suna-x-aop-ts.png'), width = 10, height = 6)
+ggsave(glue('figs/{myparam}-x-aop/{mysite}_{myparam}-x-aop-ts.png'), width = 10, height = 6)
 
-dates_both <- aop_dates %>% intersect(suna_dates) %>% as_date()
-dates_both_qa <- aop_dates %>% intersect(suna_qa_dates) %>% as_date()
+dates_both <- aop_dates %>% intersect(wq_dates) %>% as_date()
+dates_both_qa <- aop_dates %>% intersect(wq_qa_dates) %>% as_date()
   
-# subset suna data to sampling dates
-suna_df_qa_sub <- suna_df_qa %>%
+# subset WQ data to sampling dates
+wq_df_qa_sub <- wq_df_qa %>%
     mutate(sensor_position = as.character(sensor_position)) %>%
     dplyr::filter(date %in% dates_both)
   
-suna_df_all_sub <- suna_df_all %>%
+wq_df_all_sub <- wq_df_all %>%
     mutate(sensor_position = as.character(sensor_position)) %>%
     dplyr::filter(date %in% dates_both)
     
 # if there is one sensor position
-p1 <- suna_df_all_sub %>%
+p1 <- wq_df_all_sub %>%
     mutate(date = as.character(date)) %>%
-    ggplot(aes(x = time_hms, y = surfWaterNitrateMean)) +
+    ggplot(aes(x = time_hms, y = !!sym(myparam))) +
     geom_line(col = 'gray', aes(group = sensor_position)) +
-    geom_line(data = suna_df_qa_sub, aes(col = sensor_position)) +
+    geom_line(data = wq_df_all_sub, aes(col = sensor_position)) +
     theme_minimal() +
     expand_limits(y = 0) +
     xlab("UTC time") +
-    ggtitle(glue('{mysite} - SUNA on AOP flight days')) +
+    ggtitle(glue('{mysite} - {myparam} on AOP flight days')) +
     facet_wrap(vars(date)) +
     theme(legend.position = 'right')
 p1
   
-ggsave(glue('figs/suna-x-aop/{mysite}_suna-x-aop.png'), p1, width = 10, height = 6)
+ggsave(glue('figs/{myparam}-x-aop/{mysite}_{myparam}-x-aop.png'), p1, width = 10, height = 6)
 
 
 suna_x_aop_df2 <- readxl::read_excel('~/Box/data/NEON/meta/SUNA-x-AOP.xlsx',
